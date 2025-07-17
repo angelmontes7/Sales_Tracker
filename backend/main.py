f32ad08272ca804f19ae1ca5f6b28342645e4785
Angel <angelmontes7@yahoo.com>
2025-07-17 00:51:58 -0500

    comment

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
 
+# run uvicorn main:app --reload to start server
 load_dotenv()  # Loads environment variables from .env file
 
 app = FastAPI()
