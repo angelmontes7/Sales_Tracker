61f069e53427db5cf79fcbf56b413b6028caf9a8
Angel <angelmontes7@yahoo.com>
2025-07-19 16:59:18 -0500

    skeleton login page for admins

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,37 @@
+import { useState } from 'react'
+
 function Login() {
-  return <h1>Login Page</h1>
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+        console.log('Username: ', username)
+        console.log('Password', password)
+        // TODO add login API calls here
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <h2>Login</h2>
+                <input 
+                    type='text'
+                    placeholder='username'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required>
+                </input>
+                <input
+                    type='password'
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required>
+                </input>
+                <button type='submit'> Login </button>
+            </form>
+        </div>
+        
+    )
 }
 export default Login
