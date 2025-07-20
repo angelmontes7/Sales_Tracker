4563139b94973ea1ed06dd51b3aaf7c0d0250210
Angel <angelmontes7@yahoo.com>
2025-07-20 16:04:11 -0500

    completed 404 page

--- frontend/src/pages/NotFound/NotFound.tsx
+++ frontend/src/pages/NotFound/NotFound.tsx
@@ -1,4 +1,18 @@
-function NotFound() {
-  return <h1>404 Not found</h1>
-}
-export default NotFound
+import React from 'react';
+import './NotFound.css'
+interface NotFoundPageProps {}
+
+const NotFoundPage: React.FC<NotFoundPageProps> = () => {
+  return (
+    <div className="not-found-container">
+        <div className='image-container'></div>
+        <div className='error-content'>
+            <div className="error-code">404 <span className='square-dot'></span></div>
+            <h1 className="error-title">Oooooooops!</h1>
+            <button className='button-group' onClick={() => window.history.back()}> Go Back </button>
+        </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
