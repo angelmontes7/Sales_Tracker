efaab37fcdd3270fdf3519d1c3dcda5dee6f405e
Angel <angelmontes7@yahoo.com>
2025-07-21 21:22:40 -0500

    Login redesign done

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import api from "../../api"
 import { ACCESS_TOKEN, REFRESH_TOKEN} from '../../constants'
 import { useNavigate } from 'react-router-dom'
+import './Login.css'
+import logo from '../../assets/icons/discord.png';
 
 
 function Login() {
@@ -27,26 +29,38 @@ function Login() {
         }
     }
     return (
+        
         <div className='main_container'>
-            <form onSubmit={handleSubmit} className='form-container'>
-                <h2>Login</h2>
-                <input
-                    className='form-input'
-                    type='text'
-                    placeholder='username'
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required>
-                </input>
-                <input
-                    type='password'
-                    placeholder='Password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required>
-                </input>
-                <button className='form-button' type='submit'> {loading ? "Logging in..." : "Login"} </button>
-            </form>
+            <div className='login-bg'></div>
+            <div className='form-box'>
+                <div className="login-header">
+                    <div className="logo">
+                        <img src={logo}></img>
+                    </div>
+                    <h2 className="login-title">Welcome Back</h2>
+                    <p className="login-subtitle">Sign in to your account</p>
+                </div>
+                <form onSubmit={handleSubmit} className='login-form'>
+                    <h2>Login</h2>
+                    <input
+                        className='form-input'
+                        type='text'
+                        placeholder='username'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required>
+                    </input>
+                    <input
+                        className='form-input'
+                        type='password'
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required>
+                    </input>
+                    <button className='form-button' type='submit'> {loading ? "Logging in..." : "Login"} </button>
+                </form>
+            </div>
         </div>
         
     )
